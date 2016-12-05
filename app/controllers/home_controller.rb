fbc06a7296197a32387f86691b7502251093b52a
Daniel Cardenas <dan85.cardenas@gmail.com>
2016-12-04 23:19:34 -0500

    Added new home page, font styles, and projects page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,5 @@
 class HomeController < ApplicationController
   def index
+  	@basecamp = true
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,16 @@
 Rails.application.routes.draw do
 
-  root 'home#index'
+  root 'home#home_two'
 
   get '/my_work', to: 'home#my_work'
 
   get '/ondek', to: 'home#ondek'
 
   get '/begin_fm', to: 'home#begin_fm'
+
+  get '/basecamp', to: 'home#basecamp'
+
+  get '/home_two', to: 'home#home_two'
+
+  get '/projects', to: 'home#projects'
 end
